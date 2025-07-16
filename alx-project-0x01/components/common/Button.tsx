d2719acf6780f3b3cb08bce30c01431cabacb000
samuel-udoh <chukwuemekasam16@gmail.com>
2025-07-16 16:36:06 +0100

    postcard and button components

---
+++ alx-project-0x01/components/common/Button.tsx
@@ -0,0 +1,6 @@
+const Button: React.FC = () => {
+    return (
+        <button>Button</button>
+    )
+}
+export default Button
---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,6 @@
+const PostCard: React.FC = () => {
+    return (
+        <button>Button</button>
+    )
+}
+export default PostCard
